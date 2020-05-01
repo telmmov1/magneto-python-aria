fb7e657c1e800c1455a4b250ac59b8d467ac068f
magneto261290 <54003800+magneto261290@users.noreply.github.com>
2020-05-01 17:08:41 +0530

    Progress bar and changed Status UI, Looking cool

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -13,16 +13,16 @@ URL_REGEX = r"(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+"
 
 
 class MirrorStatus:
-    STATUS_UPLOADING = "Uploading"
-    STATUS_DOWNLOADING = "Downloading"
-    STATUS_WAITING = "Queued"
-    STATUS_FAILED = "Failed. Cleaning download"
-    STATUS_CANCELLED = "Cancelled"
-    STATUS_ARCHIVING = "Archiving"
+    STATUS_UPLOADING = "📤: Uploading..."
+    STATUS_DOWNLOADING = "📥: Downloading..."
+    STATUS_WAITING = ": Queued 📝"
+    STATUS_FAILED = ": Failed 🚫. Cleaning download"
+    STATUS_CANCELLED = ": Cancelled ❎"
+    STATUS_ARCHIVING = ": Archiving 🔐"
 
 
 PROGRESS_MAX_SIZE = 100 // 8
-PROGRESS_INCOMPLETE = ['▏', '▎', '▍', '▌', '▋', '▊', '▉']
+PROGRESS_INCOMPLETE = ['▣', '▣', '▣', '▣', '▣', '▣', '▣']
 
 SIZE_UNITS = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
 
@@ -77,10 +77,10 @@ def get_progress_bar_string(status):
     p = min(max(p, 0), 100)
     cFull = p // 8
     cPart = p % 8 - 1
-    p_str = '█' * cFull
+    p_str = '▣' * cFull
     if cPart >= 0:
         p_str += PROGRESS_INCOMPLETE[cPart]
-    p_str += ' ' * (PROGRESS_MAX_SIZE - cFull)
+    p_str += '▢' * (PROGRESS_MAX_SIZE - cFull)
     p_str = f"[{p_str}]"
     return p_str
 
@@ -89,16 +89,16 @@ def get_readable_message():
     with download_dict_lock:
         msg = ""
         for download in list(download_dict.values()):
-            msg += f"<i>{download.name()}</i> - "
-            msg += download.status()
+            msg += f"<b>Name</b>: <code>{download.name()}</code> "
+            msg += f"\n<b>Status</b>: <code>{download.status()}</code>"
             if download.status() != MirrorStatus.STATUS_ARCHIVING:
-                msg += f"\n<code>{get_progress_bar_string(download)} {download.progress()}</code> of " \
-                       f"{download.size()}" \
-                       f" at {download.speed()}, ETA: {download.eta()} "
+                msg += f"\n<code>{get_progress_bar_string(download)} {download.progress()}</code>" \
+                       f"\n<b>Size</b>: <code>{download.size()}</code>" \
+                       f"\n<b>Speed</b>: <code>{download.speed()}</code>\n<b>ETA</b>: <code>{download.eta()}</code> "
             if download.status() == MirrorStatus.STATUS_DOWNLOADING:
                 if hasattr(download, 'is_torrent'):
-                    msg += f"| P: {download.aria_download().connections} " \
-                           f"| S: {download.aria_download().num_seeders}"
+                    msg += f"\n<b>Seeders</b>: <code>{download.aria_download().num_seeders}</code>" \
+                           f"& <b>Peers</b>: {download.aria_download().connections} "
                 msg += f"\nGID: <code>{download.gid()}</code>"
             msg += "\n\n"
         return msg
